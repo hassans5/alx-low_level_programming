786a179e529cafd7feed937c0b107ba752ca976d
hassans5 <hassansss55966@gmail.com>
2023-08-10 18:36:26 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descripition: A C program that prints with printf gunction.
+ *
+ * Return: Always 0(success)
+*/
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
