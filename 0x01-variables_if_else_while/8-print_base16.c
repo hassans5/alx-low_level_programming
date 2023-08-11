f1688c9f7d7dfb63ee517f4a68dfdbb2d009f670
hassans5 <hassansss55966@gmail.com>
2023-08-11 10:14:39 -0700

    Add 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i < 16; i++)
-    {
-        if (i < 10)
-            putchar(i + '0');
-        else
-            putchar(i - 10 + 'a');
-    }
+for (i = 0; i < 16; i++)
+{
+if (i < 10)
+putchar(i + '0');
+else
+putchar(i - 10 + 'a');
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
