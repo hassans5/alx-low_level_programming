31a8e0937cd0e85f7f6c7ce24dc06eb8ae6ab9fa
hassans5 <hassansss55966@gmail.com>
2023-08-15 11:50:15 -0700

    a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
     if (n < 0 || n > 15)
         return;
 
-    int i, j;  // Declare variables at the beginning of the block
+    int i, j;
 
     for (i = 0; i <= n; i++)
     {
@@ -25,11 +25,24 @@ void print_times_table(int n)
             {
                 _putchar(' ');
                 _putchar(' ');
-                _putchar(result + '0');
+                _putchar(' ');
             }
             else if (result >= 10 && result < 100)
             {
                 _putchar(' ');
+                _putchar(' ');
+            }
+            else if (result >= 100)
+            {
+                _putchar(' ');
+            }
+
+            if (result < 10)
+            {
+                _putchar(result + '0');
+            }
+            else if (result >= 10 && result < 100)
+            {
                 _putchar((result / 10) + '0');
                 _putchar((result % 10) + '0');
             }
