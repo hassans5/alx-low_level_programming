3ce666ff25d182732d2fe2d6c11976d430a5412e
hassans5 <hassansss55966@gmail.com>
2023-08-15 11:41:49 -0700

    a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,8 +10,7 @@ void print_times_table(int n)
     if (n < 0 || n > 15)
         return;
 
-    int i;
-    int j;
+    int i, j;  // Declare variables at the beginning of the block
 
     for (i = 0; i <= n; i++)
     {
