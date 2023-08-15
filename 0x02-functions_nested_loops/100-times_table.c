fa824e8147b6ad1b0fc685c6072c902f0732f989
hassans5 <hassansss55966@gmail.com>
2023-08-15 14:18:07 -0700

    a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,58 +1,48 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_times_table - Prints the n times table
- * @n: The number to generate the times table for
+ * print_times_table - Prints the times table for n.
+ * @n: The times table to be printed.
  */
 void print_times_table(int n)
 {
-    if (n < 0 || n > 15)
-        return;
+if (n >= 0 && n <= 15)
+{
+int i, j, result;
 
-    int i, j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+result = i * j;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int result = i * j;
-
-            if (j != 0)
-                _putchar(',');
-
-            if (result < 10)
-            {
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(' ');
-            }
-            else if (result >= 10 && result < 100)
-            {
-                _putchar(' ');
-                _putchar(' ');
-            }
-            else if (result >= 100)
-            {
-                _putchar(' ');
-            }
-
-            if (result < 10)
-            {
-                _putchar(result + '0');
-            }
-            else if (result >= 10 && result < 100)
-            {
-                _putchar((result / 10) + '0');
-                _putchar((result % 10) + '0');
-            }
-            else if (result >= 100)
-            {
-                _putchar((result / 100) + '0');
-                _putchar(((result / 10) % 10) + '0');
-                _putchar((result % 10) + '0');
-            }
-        }
-        _putchar('\n');
-    }
+if (j == 0)
+_putchar('0');
+else
+{
+_putchar(',');
+_putchar(' ');
+if (result < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(result + '0');
+}
+else if (result < 100)
+{
+_putchar(' ');
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+}
+else
+{
+_putchar(result / 100 + '0');
+_putchar((result / 10) % 10 + '0');
+_putchar(result % 10 + '0');
+}
+}
+}
+_putchar('\n');
+}
+}
 }
