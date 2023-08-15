92bbf901db755e0d314f8027fae2044b1705895f
hassans5 <hassansss55966@gmail.com>
2023-08-15 11:08:43 -0700

    a

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -8,28 +8,28 @@
  */
 int main(void)
 {
-    int n;
+int n;
 
-    n = add(89, 9);
-    printf("%d\n", n);
+n = add(89, 9);
+printf("%d\n", n);
 
-    n = add(0, 9);
-    printf("%d\n", n);
+n = add(0, 9);
+printf("%d\n", n);
 
-    n = add(0, 0);
-    printf("%d\n", n);
+n = add(0, 0);
+printf("%d\n", n);
 
-    n = add(89, 0);
-    printf("%d\n", n);
+n = add(89, 0);
+printf("%d\n", n);
 
-    n = add(-89, 12);
-    printf("%d\n", n);
+n = add(-89, 12);
+printf("%d\n", n);
 
-    n = add(12, 12);
-    printf("%d\n", n);
+n = add(12, 12);
+printf("%d\n", n);
 
-    n = add(-89, -98);
-    printf("%d\n", n);
+n = add(-89, -98);
+printf("%d\n", n);
 
-    return (0);
+return (0);
 }
