da8aa3091aaaf176c3ef6c05fc91c4c2497a951d
hassans5 <hassansss55966@gmail.com>
2023-08-15 11:55:35 -0700

    a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,6 +18,9 @@ void print_times_table(int n)
         for (j = 0; j <= n; j++)
         {
             int result = i * j;
+            int hundreds = result / 100;
+            int tens = (result / 10) % 10;
+            int ones = result % 10;
 
             if (j != 0)
                 _putchar(',');
@@ -26,33 +29,15 @@ void print_times_table(int n)
             {
                 _putchar(' ');
                 _putchar(' ');
-                _putchar(' ');
             }
             else if (result >= 10 && result < 100)
-            {
-                _putchar(' ');
-                _putchar(' ');
-            }
-            else if (result >= 100)
             {
                 _putchar(' ');
             }
 
-            if (result < 10)
-            {
-                _putchar(result + '0');
-            }
-            else if (result >= 10 && result < 100)
-            {
-                _putchar((result / 10) + '0');
-                _putchar((result % 10) + '0');
-            }
-            else if (result >= 100)
-            {
-                _putchar((result / 100) + '0');
-                _putchar(((result / 10) % 10) + '0');
-                _putchar((result % 10) + '0');
-            }
+            _putchar(hundreds + '0');
+            _putchar(tens + '0');
+            _putchar(ones + '0');
         }
         _putchar('\n');
     }
