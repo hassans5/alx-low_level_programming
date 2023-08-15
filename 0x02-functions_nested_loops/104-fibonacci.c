664ed80e6964df1b2dc0949fa8df937e3897eced
hassans5 <hassansss55966@gmail.com>
2023-08-15 16:54:32 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -20,39 +19,6 @@ int main(void)
         b = next_term;
     }
     printf("\n");
-
-    // Create 104-fibonacci.c file and input the code
-    FILE *file = fopen("104-fibonacci.c", "w");
-    if (file != NULL)
-    {
-        fprintf(file, "#include <stdio.h>\n\n");
-        fprintf(file, "int main(void)\n");
-        fprintf(file, "{\n");
-        fprintf(file, "    unsigned long int a = 1, b = 2, next_term;\n");
-        fprintf(file, "    int i;\n\n");
-        fprintf(file, "    printf(\"%%lu, %%lu\", a, b);\n");
-        fprintf(file, "    for (i = 2; i < 98; i++)\n");
-        fprintf(file, "    {\n");
-        fprintf(file, "        next_term = a + b;\n");
-        fprintf(file, "        printf(\", %%lu\", next_term);\n");
-        fprintf(file, "        a = b;\n");
-        fprintf(file, "        b = next_term;\n");
-        fprintf(file, "    }\n");
-        fprintf(file, "    printf(\"\\n\");\n\n");
-        fprintf(file, "    return 0;\n");
-        fprintf(file, "}\n");
-
-        fclose(file);
-    }
-
-    // Compile 104-fibonacci.c
-    system("gcc -o fibonacci 104-fibonacci.c");
-
-    // Execute cat <104-fibonacci.c
-    system("cat < 104-fibonacci.c");
-
-    // Execute ./fibonacci
-    system("./fibonacci");
 
     return 0;
 }
