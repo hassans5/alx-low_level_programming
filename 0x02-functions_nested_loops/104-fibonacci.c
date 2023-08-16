69061ec0e7cfe5a37ae02a25b346d43e17f1e72c
hassans5 <hassansss55966@gmail.com>
2023-08-15 17:45:38 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 
+void add_big(unsigned int a[], unsigned int b[], unsigned int c[], int size) {
+    int carry = 0;
+    for (int i = 0; i < size; ++i) {
+        unsigned int sum = a[i] + b[i] + carry;
+        c[i] = sum % 1000000000; // Store only last 9 digits
+        carry = sum / 1000000000;
+    }
+}
+
 void print_fibonacci(int n) {
-    unsigned long long a = 1, b = 2;
+    unsigned int a[200] = {1}, b[200] = {2}, c[200] = {0};
+    int size = 1;
     
-    printf("%llu, %llu", a, b);
+    printf("%d, %d", a[0], b[0]);
     
     for (int i = 2; i < n; ++i) {
-        unsigned long long next = a + b;
-        printf(", %llu", next);
+        add_big(a, b, c, size);
         
-        a = b;
-        b = next;
+        if (c[size - 1] > 0) {
+            size++;
+        }
+        
+        for (int j = size - 1; j >= 0; --j) {
+            a[j] = b[j];
+            b[j] = c[j];
+        }
+        
+        printf(", %d", c[size - 1]);
     }
     
     printf("\n");
