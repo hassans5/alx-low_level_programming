11553f24d52fcb2fc7122cf244f532c7af05ac50
hassans5 <hassansss55966@gmail.com>
2023-08-15 18:15:45 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
-void print_fibonacci(int count) {
-    int a = 1, b = 2, next;
-    int i;
+/**
+ * print_fibonacci - Print the first 98 Fibonacci numbers.
+ * @count: The number of Fibonacci numbers to print.
+ */
+void print_fibonacci(int count)
+{
+	int a = 1, b = 2, next;
+	int i;
 
-    printf("%d, %d", a, b);
-    
-    for (i = 2; i < count; ++i) {
-        next = a + b;
-        printf(", %d", next);
-        a = b;
-        b = next;
-    }
-    printf("\n");
+	printf("%d, %d", a, b);
+
+	for (i = 2; i < count; ++i)
+	{
+		next = a + b;
+		printf(", %d", next);
+		a = b;
+		b = next;
+	}
+	printf("\n");
 }
 
-int main() {
-    print_fibonacci(98);
-    return 0;
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_fibonacci(98);
+	return (0);  /* Parentheses are added around the return value */
 }
