66576b8599ca40a9294fb67100fa1df43bfda25d
hassans5 <hassansss55966@gmail.com>
2023-08-15 18:12:13 -0700

    a

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,30 +1,21 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * jack_bauer - prints every minute of the day of Jack Bauer
- */
-void jack_bauer(void)
-{
-int h1, h2, m1, m2;
+void print_fibonacci(int count) {
+    int a = 1, b = 2, next;
+    int i;
 
-for (h1 = 0; h1 <= 2; h1++)
-{
-for (h2 = 0; h2 <= 9; h2++)
-{
-if (h1 == 2 && h2 == 4)
-break;
-for (m1 = 0; m1 <= 5; m1++)
-{
-for (m2 = 0; m2 <= 9; m2++)
-{
-_putchar('0' + h1);
-_putchar('0' + h2);
-_putchar(':');
-_putchar('0' + m1);
-_putchar('0' + m2);
-_putchar('\n');
+    printf("%d, %d", a, b);
+    
+    for (i = 2; i < count; ++i) {
+        next = a + b;
+        printf(", %d", next);
+        a = b;
+        b = next;
+    }
+    printf("\n");
 }
-}
-}
-}
+
+int main() {
+    print_fibonacci(98);  /* Print the first 98 Fibonacci numbers */
+    return 0;
 }
