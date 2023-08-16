662d2eeac8c83fd085b834be1f4d1e7d95d443bf
hassans5 <hassansss55966@gmail.com>
2023-08-15 18:22:07 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
+#include <gmp.h>
 
-/**
- * print_fibonacci - Print the first 98 Fibonacci numbers.
- * @count: The number of Fibonacci numbers to print.
- */
-void print_fibonacci(int count)
-{
-	int a = 1, b = 2, next;
-	int i;
+void print_fibonacci(int count) {
+    mpz_t a, b, next;
+    mpz_init_set_ui(a, 1);
+    mpz_init_set_ui(b, 2);
+    mpz_init(next);
 
-	printf("%d, %d", a, b);
+    printf("%s, %s", mpz_get_str(NULL, 10, a), mpz_get_str(NULL, 10, b));
 
-	for (i = 2; i < count; ++i)
-	{
-		next = a + b;
-		printf(", %d", next);
-		a = b;
-		b = next;
-	}
-	printf("\n");
+    for (int i = 2; i < count; ++i) {
+        mpz_add(next, a, b);
+        printf(", %s", mpz_get_str(NULL, 10, next));
+        mpz_set(a, b);
+        mpz_set(b, next);
+    }
+
+    mpz_clear(a);
+    mpz_clear(b);
+    mpz_clear(next);
+    printf("\n");
 }
 
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_fibonacci(98);
-	return (0);  /* Parentheses are added around the return value */
+int main(void) {
+    print_fibonacci(98);
+    return 0;
 }
