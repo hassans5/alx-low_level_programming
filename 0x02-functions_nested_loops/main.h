928f66d6dc7bc37e5575a15325964edb50362118
hassans5 <hassansss55966@gmail.com>
2023-08-16 07:12:07 -0700

    a

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_alphabet_x10(void);
+/* Declaration of _putchar function */
 int _putchar(char c);
+
+/* Declaration of print_alphabet function */
+void print_alphabet(void);
 
 #endif /* MAIN_H */
