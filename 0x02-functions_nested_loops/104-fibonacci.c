143daf84d9725ee459780a3d91cdab963d06c684
hassans5 <hassansss55966@gmail.com>
2023-08-15 19:18:12 -0700

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,18 +9,18 @@
  */
 int numLength(int num)
 {
-    int length = 0;
+int length = 0;
 
-    if (!num)
-        return (1);
+if (!num)
+return (1);
 
-    while (num)
-    {
-        num = num / 10;
-        length += 1;
-    }
+while (num)
+{
+num = num / 10;
+length += 1;
+}
 
-    return (length);
+return (length);
 }
 
 /**
@@ -33,37 +33,37 @@ int numLength(int num)
  */
 int main(void)
 {
-    int count, initials;
-    unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0,
-                  sumo = 0;
+int count, initials;
+unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0,
+sumo = 0;
 
-    for (count = 1; count <= 98; count++)
-    {
-        if (f1o > 0)
-            printf("%lu", f1o);
+for (count = 1; count <= 98; count++)
+{
+if (f1o > 0)
+printf("%lu", f1o);
 
-        initials = numLength(mx) - 1 - numLength(f1);
-        while (f1o > 0 && initials > 0)
-        {
-            printf("%d", 0);
-            initials--;
-        }
+initials = numLength(mx) - 1 - numLength(f1);
+while (f1o > 0 && initials > 0)
+{
+printf("%d", 0);
+initials--;
+}
 
-        printf("%lu", f1);
+printf("%lu", f1);
 
-        sum = (f1 + f2) % mx;
-        sumo = f1o + f2o + (f1 + f2) / mx;
+sum = (f1 + f2) % mx;
+sumo = f1o + f2o + (f1 + f2) / mx;
 
-        f1o = f2o;
-        f2o = sumo;
-        f1 = f2;
-        f2 = sum;
+f1o = f2o;
+f2o = sumo;
+f1 = f2;
+f2 = sum;
 
-        if (count != 98)
-            printf(", ");
-        else
-            printf("\n");
-    }
+if (count != 98)
+printf(", ");
+else
+printf("\n");
+}
 
-    return (0);
+return (0);
 }
