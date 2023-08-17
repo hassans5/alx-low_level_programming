4e23aa4a5e4195c359f98c13bd5f83f23bbaeaf3
hassans5 <hassansss55966@gmail.com>
2023-08-17 04:37:45 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,5 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * main - Fizz-Buzz test
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 	int i;
@@ -12,41 +7,14 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			print_str("FizzBuzz ");
+			printf("FizzBuzz ");
 		else if (i % 3 == 0)
-			print_str("Fizz ");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			print_str("Buzz ");
+			printf("Buzz ");
 		else
-			print_int(i);
-		_putchar(' ');
+			printf("%d ", i);
 	}
-	_putchar('\n');
+	printf("\n");
 	return (0);
-}
-
-/**
- * print_str - Prints a string
- * @str: The string to print
- */
-void print_str(char *str)
-{
-	while (*str)
-		_putchar(*str++);
-}
-
-/**
- * print_int - Prints an integer
- * @n: The integer to print
- */
-void print_int(int n)
-{
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-		print_int(n / 10);
-	_putchar(n % 10 + '0');
 }
