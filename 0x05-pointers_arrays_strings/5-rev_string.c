9aed4d01b5bcffc2ac19ce4fbc12a1c758738b75
hassans5 <hassansss55966@gmail.com>
2023-08-20 22:28:10 -0700

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,19 +6,19 @@
  */
 void rev_string(char *s)
 {
-    int length = 0;
-    char temp;
-    int i; /* Declare the loop variable here */
+	int length = 0;
+	char temp;
+	int i; /* Declare the loop variable here */
 
-    /* Calculate the length of the string */
-    while (s[length] != '\0')
-        length++;
+	/* Calculate the length of the string */
+	while (s[length] != '\0')
+		length++;
 
-    /* Swap characters from start and end towards the middle */
-    for (i = 0; i < length / 2; i++) /* Use the loop variable here */
-    {
-        temp = s[i];
-        s[i] = s[length - i - 1];
-        s[length - i - 1] = temp;
-    }
+	/* Swap characters from start and end towards the middle */
+	for (i = 0; i < length / 2; i++) /* Use the loop variable here */
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
 }
