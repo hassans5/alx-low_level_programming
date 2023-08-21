69db33b5c1497f3e7ea672e43334c3c45c393772
hassans5 <hassansss55966@gmail.com>
2023-08-20 22:46:12 -0700

    a

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -16,5 +16,9 @@ int _atoi(char *s);
 void _puts(char *str);
 int _putchar(char c);
 void _puts(char *str);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 
 #endif /* MAIN_H */
