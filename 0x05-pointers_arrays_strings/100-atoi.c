c96622e0380b75091d0dd90a0eab7402a0c2a3c2
hassans5 <hassansss55966@gmail.com>
2023-08-20 23:49:51 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,32 @@
-/**
- * _atoi - converts a string to an integer
- * @s: string to convert
- *
- * Return: integer value of string
- */
+#include <limits.h>
+
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
+    int i = 0;
+    int sign = 1;
+    int result = 0;
 
-	/* Skip any non-digit characters */
-	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
-	}
+    /* Skip any non-digit characters */
+    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        i++;
+    }
 
-	/* Convert the string of digits to an integer */
-	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+    /* Convert the string of digits to an integer */
+    while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+    {
+        /* Check for potential overflow */
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+        {
+            result = (sign == 1) ? INT_MAX : INT_MIN;
+            break;
+        }
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
 
-	/* Return the result with the appropriate sign */
-	return (result * sign);
+    /* Return the result with the appropriate sign */
+    return (result * sign);
 }
