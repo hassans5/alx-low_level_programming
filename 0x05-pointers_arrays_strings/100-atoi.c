e12b18f1166af033b677624fd2779083157e5479
hassans5 <hassansss55966@gmail.com>
2023-08-20 23:24:13 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -26,20 +25,9 @@ int _atoi(char *s)
     /* Convert the digits to an integer */
     while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
     {
-        /* Check for signed integer overflow */
-        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-        {
-            result = (sign == 1) ? INT_MAX : INT_MIN;
-            break;
-        }
-
         result = result * 10 + (s[i] - '0');
         i++;
     }
-
-    /* Check for signed integer overflow */
-    if (result == INT_MIN && sign == 1)
-        return INT_MAX;
 
     return result * sign;
 }
