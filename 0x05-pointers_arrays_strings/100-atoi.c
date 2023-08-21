fe63196a532d14fbb950ee7beb73d0dc9753e6a6
hassans5 <hassansss55966@gmail.com>
2023-08-20 23:28:08 -0700

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,33 +9,33 @@
  */
 int _atoi(char *s)
 {
-    int i, sign, result;
+	int i, sign, result;
 
-    i = 0;
-    sign = 1;
-    result = 0;
+	i = 0;
+	sign = 1;
+	result = 0;
 
-    /* Skip any non-digit characters */
-    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        i++;
-    }
+	/* Skip any non-digit characters */
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
 
-    /* Convert the digits to an integer */
-    while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
-    {
-        /* Check for signed integer overflow */
-        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
-        {
-            result = (sign == 1) ? INT_MAX : INT_MIN;
-            break;
-        }
+	/* Convert the digits to an integer */
+	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+	{
+		/* Check for signed integer overflow */
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+		{
+			result = (sign == 1) ? INT_MAX : INT_MIN;
+			break;
+		}
 
-        result = result * 10 + (s[i] - '0');
-        i++;
-    }
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 
-    return result * sign;
+	return result * sign;
 }
