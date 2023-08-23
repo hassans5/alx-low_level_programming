a18a954f56abe04f00e97a00e71ade1367cb3e50
hassans5 <hassansss55966@gmail.com>
2023-08-23 11:04:18 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * isLower - Checks if a character is a lowercase letter.
+ * isLower - Determines whether a character is lowercase.
  * @c: The character to be checked.
  *
  * Return: 1 if the character is lowercase, 0 otherwise.
@@ -13,47 +13,53 @@ int isLower(char c)
 }
 
 /**
- * isDelimiter - Checks if a character is a delimiter.
+ * isDelimiter - Determines whether a character is a delimiter.
  * @c: The character to be checked.
  *
  * Return: 1 if the character is a delimiter, 0 otherwise.
  */
 int isDelimiter(char c)
 {
-	char delimiters[] = " \t\n,.!?\"(){}";
+	int i;
+	char delimiter[] = " \t\n, .1?\" () ()";
 
-	for (int i = 0; delimiters[i]; i++)
+	for (i = 0; i < 12; i++)
 	{
-		if (c == delimiters[i])
+		if (c == delimiter[i])
 			return (1);
 	}
 	return (0);
 }
 
 /**
- * cap_string - Capitalizes all words in a string.
+ * cap_string - Capitalizes all words of a string.
  * @s: The input string.
  *
  * Return: Pointer to the modified string.
  */
 char *cap_string(char *s)
 {
-	int capitalize_next = 1;
+	char *ptr = s;
+	int foundDelimit = 1;
 
-	for (int i = 0; s[i]; i++)
+	while (*s)
 	{
-		if (isDelimiter(s[i]))
-			capitalize_next = 1;
-		else if (capitalize_next && isLower(s[i]))
+		if (isDelimiter(*s))
 		{
-			s[i] -= 32;
-			capitalize_next = 0;
+			foundDelimit = 1;
+		}
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
 		}
 		else
 		{
-			capitalize_next = 0;
+			foundDelimit = 0;
 		}
+
+		s++;
 	}
 
-	return (s);
+	return (ptr);
 }
