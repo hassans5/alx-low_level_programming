e0db1f15c1b0e9bdde09417f8d4df2ff00a6b817
hassans5 <hassansss55966@gmail.com>
2023-08-23 11:48:18 -0700

    a

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,7 +8,6 @@ int main(void)
 
     a[2] = 1024;
     p = &n;
-
     /*
      * write your line of code here...
      * Remember:
@@ -18,7 +17,7 @@ int main(void)
      * - you are not allowed to code anything else than this line of code
      */
     *(p + 5) = 98;
-
+    *(a + 2) = 98; /* Add this line to change the value of a[2] to 98 */
     /* ...so that this prints 98\n */
     printf("a[2] = %d\n", a[2]);
     return (0);
