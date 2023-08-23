b88c9f800bbf8aa10fe1ec237d83cc0d8ff721f3
hassans5 <hassansss55966@gmail.com>
2023-08-23 10:55:18 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,54 +1,57 @@
-#include <stdbool.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * isLower - determines whether a character is lowercase
- * @c: character
- * Return: 1 if c is lowercase, 0 otherwise
-*/
+ * isLower - Checks if a character is a lowercase letter.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise.
+ */
 int isLower(char c)
 {
-	return (c >= 97 && c <= 122);
+	return (c >= 'a' && c <= 'z');
 }
 
 /**
- * isDelimiter - determines whether a character is a delimiter
- * @c: character
- * Return: 1 if c is a delimiter, 0 otherwise
-*/
+ * isDelimiter - Checks if a character is a delimiter.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if the character is a delimiter, 0 otherwise.
+ */
 int isDelimiter(char c)
 {
-	int i;
-	char delimiter[] = " \t\n,.;!?\"(){}";
+	char delimiters[] = " \t\n,.!?\"(){}";
 
-	for (i = 0; i < 12; i++)
+	for (int i = 0; delimiters[i]; i++)
 	{
-		if (c == delimiter[i])
+		if (c == delimiters[i])
 			return (1);
 	}
 	return (0);
 }
 
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string
- * Return: string with capitalized words
-*/
+ * cap_string - Capitalizes all words in a string.
+ * @s: The input string.
+ *
+ * Return: Pointer to the modified string.
+ */
 char *cap_string(char *s)
 {
-	bool new_word = true;
-	int i;
+	int capitalize_next = 1;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (int i = 0; s[i]; i++)
 	{
-		if (new_word && s[i] >= 'a' && s[i] <= 'z')
+		if (isDelimiter(s[i]))
+			capitalize_next = 1;
+		else if (capitalize_next && isLower(s[i]))
 		{
-			s[i] -= ('a' - 'A');
-			new_word = false;
+			s[i] -= 32;
+			capitalize_next = 0;
 		}
-		else if (isDelimiter(s[i]))
+		else
 		{
-			new_word = true;
+			capitalize_next = 0;
 		}
 	}
 
