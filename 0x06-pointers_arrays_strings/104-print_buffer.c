730ebedcfc49b99584b29af293ed1a8a1bf5c43c
hassans5 <hassansss55966@gmail.com>
2023-08-23 17:27:53 -0700

    a

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 /**
- * isPrintableASCII - Determines if a character is printable ASCII
- * @n: The character's ASCII value
- *
- * Description: This function checks if a given ASCII value is printable.
- * Returns: 1 if printable, 0 if not printable
- */
+ * isPrintableASCII - Determines if n is a printable ASCII char
+ * @n: integet
+ * Return: 1 if teue, 0 if false
+*/
+
 int isPrintableASCII(int n)
 {
-    return (n >= 32 && n <= 126);
+	return (n >= 32 && n <= 126);
 }
 
 /**
@@ -18,26 +17,26 @@ int isPrintableASCII(int n)
  * @b: Pointer to the string
  * @start: Starting position
  * @end: Ending position
- *
  * Description: This function prints the hexadecimal values of a string
  *              within the specified range.
- */
+*/
+
 void printHexes(char *b, int start, int end)
 {
-    int i = 0;
+	int i = 0;
 
-    while (i < 10)
-    {
-        if (i < end)
-            printf("%02x", *(b + start + i));
-        else
-            printf("  ");
+	while (i < 10)
+	{
+		if (i < end)
+			printf("%02x", *(b + start + i));
+		else
+			printf("  ");
 
-        if (i % 2)
-            printf(" ");
+		if (i % 2)
+			printf(" ");
 
-        i++;
-    }
+		i++;
+	}
 }
 
 /**
@@ -45,50 +44,50 @@ void printHexes(char *b, int start, int end)
  * @b: Pointer to the string
  * @start: Starting position
  * @end: Ending position
- *
  * Description: This function prints the ASCII values of a string within
  *              the specified range. Non-printable characters are replaced
  *              with '.'
- */
+*/
+
 void printASCII(char *b, int start, int end)
 {
-    int ch, i = 0;
+	int ch, i = 0;
 
-    while (i < end)
-    {
-        ch = *(b + i + start);
-        if (!isPrintableASCII(ch))
-            ch = 46;
-        printf("%c", ch);
-        i++;
-    }
+	while (i < end)
+	{
+		ch = *(b + i + start);
+		if (!isPrintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
 }
 
 /**
  * print_buffer - Prints a buffer with specific formatting
  * @b: Pointer to the buffer
  * @size: Size of the buffer
- *
  * Description: This function prints the content of a buffer in a specific format.
  *              The output displays the hexadecimal content of the buffer along
  *              with ASCII characters.
- */
+*/
+
 void print_buffer(char *b, int size)
 {
-    int start, end;
+	int start, end;
 
-    if (size > 0)
-    {
-        for (start = 0; start < size; start += 10)
-        {
-            end = (size - start < 10) ? (size - start) : 10;
-            printf("%08x: ", start);
-            printHexes(b, start, end);
-            printASCII(b, start, end);
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? (size - start) : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
 
-            printf("\n");
-        }
-    }
-    else
-        printf("\n");
+			printf("\n");
+		}
+	}
+	else
+		printf("\n");
 }
