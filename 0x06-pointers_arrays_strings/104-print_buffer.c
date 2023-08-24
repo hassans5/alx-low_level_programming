7e8f1836b0ca8ba5e29e52268718606b3dd4b4e3
hassans5 <hassansss55966@gmail.com>
2023-08-23 17:14:54 -0700

    a

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -25,6 +25,7 @@ int isPrintableASCII(int n)
 void printHexes(char *b, int start, int end)
 {
     int i = 0;
+
     while (i < 10)
     {
         if (i < end)
@@ -52,13 +53,12 @@ void printHexes(char *b, int start, int end)
 void printASCII(char *b, int start, int end)
 {
     int ch, i = 0;
+
     while (i < end)
     {
         ch = *(b + i + start);
-
         if (!isPrintableASCII(ch))
-            ch = 46; // ASCII value for '.'
-
+            ch = 46;
         printf("%c", ch);
         i++;
     }
@@ -82,7 +82,6 @@ void print_buffer(char *b, int size)
         for (start = 0; start < size; start += 10)
         {
             end = (size - start < 10) ? (size - start) : 10;
-
             printf("%08x: ", start);
             printHexes(b, start, end);
             printASCII(b, start, end);
@@ -91,7 +90,5 @@ void print_buffer(char *b, int size)
         }
     }
     else
-    {
         printf("\n");
-    }
 }
