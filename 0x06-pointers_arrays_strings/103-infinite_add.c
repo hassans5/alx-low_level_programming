af5a4a24041eba911a18eabab39036b7d1509f09
hassans5 <hassansss55966@gmail.com>
2023-08-23 19:10:09 -0700

    a

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,48 +1,59 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
- * infinite_add - Adds two numbers represented as strings
- * @n1: First number as a string
- * @n2: Second number as a string
- * @r: Buffer to store the result
- * @size_r: Size of the buffer
- *
- * Return: Pointer to the result (r) or 0 if result cannot fit in r
+ * infinite_add - Adds two numbers stored as strings.
+ * @n1: First number as a string.
+ * @n2: Second number as a string.
+ * @r: Buffer to store the result of addition.
+ * @size_r: Size of the result buffer.
+ * Return: Pointer to the result buffer.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = strlen(n1);
-	int len2 = strlen(n2);
-	int carry = 0;
-	int max_len = len1 > len2 ? len1 : len2;
-	int i, j, k;
+	int i = 0, j = 0, k, carry = 0, f, s, d = 0;
 
-	if (max_len + 1 > size_r)
-		return (0);
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
 
-	for (i = len1 - 1, j = len2 - 1, k = 0;
-		i >= 0 || j >= 0 || carry; i--, j--, k++)
+	if (i > j)
+		k = i;
+	else
+		k = j;
+
+	i--;
+	j--;
+
+	for (; k >= 0; k--)
 	{
-		int digit1 = i >= 0 ? n1[i] - '0' : 0;
-		int digit2 = j >= 0 ? n2[j] - '0' : 0;
-		int sum = digit1 + digit2 + carry;
+		if (i >= 0)
+			f = n1[i] - '0';
+		else
+			f = 0;
 
-		carry = sum / 10;
-		r[k] = (sum % 10) + '0';
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
+			s = 0;
+
+		r[k] = (f + s + carry) % 10 + '0';
+		carry = (f + s + carry) / 10;
+
+		i--;
+		j--;
 	}
 
-	r[k] = '\0';
+	if (carry == 1)
+		r[0] = carry + '0';
 
-	for (i = 0, j = k - 1; i < j; i++, j--)
-	{
-		char temp;
+	if (k < 0 && size_r <= (k * -1))
+		return NULL;
 
-		temp = r[i];
-		r[i] = r[j];
-		r[j] = temp;
-	}
+	for (i = size_r - 1; i >= 0; i--)
+		r[i + 1] = r[i];
 
-	return (r);
+	r[0] = carry + '0';
+
+	return r;
 }
