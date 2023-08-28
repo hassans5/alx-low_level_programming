78a54d23a6c2404d983c186a727137dc5a50f98e
hassans5 <hassansss55966@gmail.com>
2023-08-28 03:11:36 -0700

    a

---
+++ 0x07-pointers_arrays_strings/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+    char buffer[] = "Your test string goes here";
+    print_buffer(buffer, sizeof(buffer));
+    return (0);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+char *cap_string(char *);
+char *rot13(char *);
+void print_number(int n);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
