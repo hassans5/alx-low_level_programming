558aad3a06e616e628e9240402c0e95edb1ba3cc
hassans5 <hassansss55966@gmail.com>
2023-08-28 04:05:09 -0700

    a

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,5 +15,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 void set_string(char **s, char *to);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
